3fafecd6ac72697516e29d8c98cb257a8795a2d1
M0onFall <carvalhovincius71@gmail.com>
2021-09-18 19:53:06 -0300

    Finalização

--- js/menu.js
+++ js/menu.js
@@ -8,15 +8,35 @@
    
    $('a').click(function(e){
        e.preventDefault()
-     
-                
+
        var url = $(this).attr('href')
        $('#' + url).show()
-      
-     if(url == "contact"){
-       $('a').attr('')
-     }
-      
+     
+       switch(url){
+        case 'home':
+          $('#contact').hide()
+          $('#about').hide()
+          $('#action').hide()
+          break
+        case 'contact':
+          $('#home').hide()
+          $('#about').hide()
+          $('#action').hide()
+          break
+        case 'about':
+          $('#contact').hide()
+          $('#home').hide()
+          $('#action').hide()
+          break
+        case 'action':
+          $('#contact').hide()
+          $('#home').hide()
+          $('#about').hide()
+          break
+       }        
+       
+
+
        
    })
 
